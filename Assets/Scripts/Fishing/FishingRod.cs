dc696a3fd38ee35e118c2ff3eed838fbdc8f7f0f
KIRVAS <133602486+KIRVASgames@users.noreply.github.com>
2024-06-08 02:01:50 +0300

    FishingPoint min distance bug fixed

--- Assets/Scripts/Fishing/FishingRod.cs
+++ Assets/Scripts/Fishing/FishingRod.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using System.Collections.Generic;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -128,17 +130,38 @@ namespace ShipMotorika
 
             if (hits.Length > 0)
             {
-                foreach (Collider2D hit in hits)
+                List<FishingPoint> fishingPoints = new ();
+                
+                foreach (var hit in hits)
                 {
-                    if (hit.TryGetComponent<FishingPoint>(out var fishingPoint))
+                    if (hit.gameObject.TryGetComponent<FishingPoint>(out var point))
                     {
-                        if (!_isTriggered)
-                        {
-                            _isTriggered = true;
-                            _activeFishingPoint = fishingPoint;
-                            _activeFishingPoint.SetActive(true);
-                            OnFishingPlaceNearby(true);
-                        }
+                        fishingPoints.Add(point);
+                    }
+                }
+
+                FishingPoint closestFishingPoint = null;
+                float minDistance = Mathf.Infinity;
+
+                foreach (var fishingPoint in fishingPoints)
+                {                   
+                    float distance = Vector2.Distance(transform.position, fishingPoint.transform.position);
+                    
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        closestFishingPoint = fishingPoint;
+                    }
+                }
+
+                if (closestFishingPoint != null)
+                {
+                    if (!_isTriggered)
+                    {
+                        _isTriggered = true;
+                        _activeFishingPoint = closestFishingPoint;
+                        _activeFishingPoint.SetActive(true);
+                        OnFishingPlaceNearby(true);
                     }
                 }
             }
