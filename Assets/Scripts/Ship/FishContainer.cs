8c99a762116a16729f2f66a0e096ffd992bb11e5
KIRVAS <133602486+KIRVASgames@users.noreply.github.com>
2024-06-08 02:45:17 +0300

    Add basic scripts for Market mechanics

---
+++ Assets/Scripts/Ship/FishContainer.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace ShipMotorika
+{
+    /// <summary>
+    /// Склад для пойманной рыбы на корабле.
+    /// </summary>
+    public class FishContainer : MonoBehaviour
+    {
+        /// <summary>
+        /// Суммарный вес пойманной рыбы.
+        /// </summary>
+        [SerializeField] private int _totalFishWeight;
+        public int Weight => _totalFishWeight;
+
+        /// <summary>
+        /// Суммарна¤ стоимость пойманной рыбы.
+        /// </summary>
+        [SerializeField] private int _totalFishCost;
+        private int Cost => _totalFishCost;
+
+        public void ChangeWeightAmount(int amount)
+        {
+            _totalFishWeight += amount;
+
+            if (_totalFishWeight < 0)
+            {
+                _totalFishWeight = 0;
+            }
+        }
+
+        public void ChangeCostAmount(int amount)
+        {
+            _totalFishCost += amount;
+
+            if ( _totalFishCost < 0)
+            {
+                _totalFishCost = 0;
+            }
+        }
+    }
+}
